68cffad56f4a4ccbd995217fc0175cb206b2a95d
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-12-01 14:31:06 +0300

    Add files via upload

--- displayLikes.test.js
+++ displayLikes.test.js
@@ -6,4 +6,16 @@ describe('displayLikes()', () => {
       expect(displayLikes('')).toBe('No one likes this')
   });
 
+  test('if only 1 person likes', () => {
+    expect(displayLikes(['Arno'])).toBe('Arno likes this')
+  });
+
+  test('if only 2 person like', () => {
+    expect(displayLikes(['Arno', 'Jessie'])).toBe('Arno and Jessie likes this')
+  });
+
+  test('if more than 2 person like', () => {
+    expect(displayLikes(['Arno', 'Jessie', 'Donald', 'Max'])).toBe('Arno, Jessie and other 2 likes this')
+  });
+
 })
