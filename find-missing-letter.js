8e34ecbf14f8c99f43922470a36bea0dd0fe8d14
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2024-03-01 12:00:40 +0300

    Add files via upload
    Solution using char code

--- find-missing-letter.js
+++ find-missing-letter.js
@@ -1,23 +1,45 @@
 
 // Solution using alphabet
-function findMissingLetter(arr) {
+// function findMissingLetter(arr) {
 
-    // Alphabet with lowercase and uppercase letters
-    const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+//     // Alphabet with lowercase and uppercase letters
+//     const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-    // Starting point
-    const startIndex = alphabet.indexOf(arr[0])
+//     // Starting point
+//     const startIndex = alphabet.indexOf(arr[0])
+
+//     for(let i = 0; i < arr.length; i++){
+//         if( arr[i] !== alphabet[startIndex + i]){
+//             return alphabet[startIndex + i];
+//         };
+//     }
+
+//     return '';
+// }
+
+
+
+// Solution using char unicode
+function findMissingLetter(arr){
+    let start = arr[0].charCodeAt();
 
     for(let i = 0; i < arr.length; i++){
-        if( arr[i] !== alphabet[startIndex + i]){
-            return alphabet[startIndex + i];
-        };
+
+        // Current charCode value
+        const current = arr[i].charCodeAt(0);
+        console.log(current);
+
+        if(current - start > 1){
+            return String.fromCharCode(start + 1);
+        }
+
+        start = current;
     }
 
     return '';
-}
 
-const res = findMissingLetter(['a', 'b', 'c', 'f', 'g']);
-console.log(res);
+}
+console.log(findMissingLetter(['a', 'b', 'd']));
+
 
 module.exports = findMissingLetter;
