097991eac883627f2bc45b4082168d10d22ed612
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2024-03-03 15:37:08 +0300

    Add files via upload
    Are all chars unique. Solution with set

--- are-all-chars-unique.js
+++ are-all-chars-unique.js
@@ -1,21 +1,46 @@
 
 
+
+    // Solution with empty object
+// const areAllCharactersUnique = (string) => {
+    
+//     const charCount = {
+        
+//     }
+
+//     for (let i = 0; i < string.length; i++) {
+//         const char = string[i];
+        
+//         // If not all chars are unique return false
+//         if(charCount[char]){
+//             return false
+//         }
+
+//         // Add new value to the initial empty object and set this value as true
+//         charCount[char] = true;
+//     }
+
+//     return true
+// }
+
+
+
+// Solution with Set
 const areAllCharactersUnique = (string) => {
     
-    const charCount = {
-        
-    }
+    const charSet = new Set();        
+
 
     for (let i = 0; i < string.length; i++) {
+
         const char = string[i];
         
-        // If not all chars are unique return false
-        if(charCount[char]){
+        // If not all chars in the set are unique return false
+        if(charSet.has(char)){
             return false
         }
-
-        // Add new value to the initial empty object and set this value as true
-        charCount[char] = true;
+        // Add new value in the set
+        charSet.add(char)
     }
 
     return true
