c0c2e4bbf332e372ea3aafccffcdcb7c29b1588e
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2024-03-03 18:32:48 +0300

    Add files via upload
    Refactored

--- sum-of-even-squares.js
+++ sum-of-even-squares.js
@@ -1,18 +1,31 @@
 
 
+// function sumOfEvenSquares(arr) {
+
+//     // Filter only Even Numbers
+//     const filteredArray = arr.filter((number) => number % 2 === 0 );
+    
+//     let sum = 0
+
+//     filteredArray.forEach((item) => {
+//         sum += item ** 2;
+//     })
+
+//     return sum;
+// }
+
+
 function sumOfEvenSquares(arr) {
 
-    // Filter only Even Numbers
-    const filteredArray = arr.filter((number) => number % 2 === 0 );
-    
-    let sum = 0
-
-    filteredArray.forEach((item) => {
-        sum += item ** 2;
-    })
-
-    return sum;
-}
+        // Filter only Even Numbers
+        const evenSquares = arr.filter((number) => number % 2 === 0 )
+                               .map((num) => num ** 2)
+                               .reduce((sum, squaredNumber) => sum + squaredNumber, 0);
+                                
+        console.log(evenSquares);
+        
+        return evenSquares;
+    }
 
 
 module.exports = sumOfEvenSquares;
