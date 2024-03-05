4cdaf6a6bdd36949f318b1c52550fee0d15ef324
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2024-03-05 14:31:00 +0300

    Add files via upload
    Password Validator. Password Length and Password UpperCase validation

--- password-validator.js
+++ password-validator.js
@@ -3,13 +3,38 @@
 
 function validatePassword(password) {
 
-    if(password.length < 8) {
-        return false
+    // Password must be at least 8 characters long
+    const isLengthValid = password.length >= 8;
+
+    // Password must contain at least 1 UpperCase letter
+    const hasUpperCase = password.split('')
+                                 .some((char) => char === char.toUpperCase() && char !== char.toLowerCase());
+
+    console.log(hasUpperCase);
+
+    // Password must contain at least 1 LowerCase letter
+    const hasLowerCase= '';
+
+    // Password must contain at least 1 digit
+    const hasDigit= '';
+
+    if(isLengthValid && hasUpperCase && hasLowerCase && hasDigit){
+        return true
     }
 
-    return true
+    if(!isLengthValid){
+        return ('Password must be at least 8 characters long');
+    } else if(!hasUpperCase){
+        return ('Password must contain at least 1 UpperCase letter');
+    } else if (!hasLowerCase){
+        return ('Password must contain at least 1 LowerCase letter');
+    } else if (hasDigit){
+        return ('Password must contain at least 1 digit');
+    }
+
 }
 
-console.log(validatePassword('123456'));
+console.log(validatePassword('123456789'));
+console.log(validatePassword('a123456789'));
 
 module.exports = validatePassword;
--- password-validator.test.js
+++ password-validator.test.js
@@ -2,10 +2,11 @@ const validatePassword = require('./password-validator');
 
 describe('Password Validation', () => {
   
+        // Must return true
 	test('Password must be at least 8 characters long', () => {
         expect(validatePassword('12345678')).toBe(true);
     })
-
+        // Must return false
 	test('Password must be at least 8 characters long', () => {
         expect(validatePassword('123')).toBe(false);
     })
