d2a4ec3bf6c69cd2cadf30f24f2764cf47bc609c
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2024-03-05 14:01:14 +0300

    Add files via upload
    Hashtag Generator. Solution with reduce();

--- hashtag-generator.js
+++ hashtag-generator.js
@@ -1,6 +1,32 @@
 
 
+    // Simple Solution
+// function generateHashtag(string) {
 
+//     // If provided string longer than 140 or empty return false
+//     if(string.trim().length > 140 || string.trim() === '') {
+//         console.log(string.trim().length);
+//         return false;
+//     };
+
+//     // Split(Separate) every word from the provided string
+//     const splittedString =  string.trim().split(' ');
+
+//     // Replace every first letter to uppercase
+//     for(let i = 0; i < splittedString.length; i++){
+//         splittedString[i] = splittedString[i][0].toUpperCase() + splittedString[i].substr(1)
+        
+//     }
+
+//     // Join (Unite) every separated word in one string
+//     const joinedWords =  splittedString.join('');
+    
+
+//     return '#' +joinedWords;
+// }
+
+
+    // Solution with reduce();
 function generateHashtag(string) {
 
     // If provided string longer than 140 or empty return false
@@ -9,20 +35,14 @@ function generateHashtag(string) {
         return false;
     };
 
-    // Split(Separate) every word from the provided string
-    const splittedString =  string.trim().split(' ');
-
-    // Replace every first letter to uppercase
-    for(let i = 0; i < splittedString.length; i++){
-        splittedString[i] = splittedString[i][0].toUpperCase() + splittedString[i].substr(1)
-        
-    }
-
-    // Join (Unite) every separated word in one string
-    const joinedWords =  splittedString.join('');
+    // First we separate every single world and make it array element then we join them with reduce()
+    const hashtag = string.split(' ')
+                          .reduce((tag, word) => {
+                          return  tag + word.charAt(0).toUpperCase() + word.substr(1);
+                          }, '#');
     
-
-    return '#' +joinedWords;
+    return hashtag;                      
+                        
 }
 
 module.exports = generateHashtag;
