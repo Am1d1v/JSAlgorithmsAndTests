4f0183e86ba20b8db7932ce76c6635405cf0c779
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2024-03-06 17:08:41 +0300

    Add files via upload
    Unwinding

--- sum-up-to.js
+++ sum-up-to.js
@@ -1,13 +1,32 @@
 
 
+// function sumUpTo(num) {
+
+//     if(num === 1){
+//         return 1;
+//     }
+
+//     return num + sumUpTo(num - 1);
+
+// }
+
+
+    // Last In First Out
 function sumUpTo(num) {
 
     if(num === 1){
+        console.log(`sumUpTo(${num}) return 1`);
         return 1;
     }
 
-    return num + sumUpTo(num - 1);
+    console.log(`sumUpTo(${num}) calls sumUpTo(${num - 1}) + ${num}`);
 
+    const res = num + sumUpTo(num - 1);
+    console.log(`sumUpTo(${num}) returns ${res}`);
+
+    return res;
 }
+
+console.log(sumUpTo(3));
 
 module.exports = sumUpTo;
--- sum-up-to.test.js
+++ sum-up-to.test.js
@@ -1,7 +1,8 @@
-const sumUpTo = require('../02-unwinding/sum-up-to')
+const sumUpTo = require('./sum-up-to')
 
 test('Summing up positive integers', () => {
   
   expect(sumUpTo(3)).toBe(6);
+  expect(sumUpTo(5)).toBe(15);
 
 });
