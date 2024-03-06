bbda952713948646414abe7e5353f69f2bc30155
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2024-03-06 16:58:29 +0300

    Add files via upload
    Unwinding

---
+++ sum-up-to.js
@@ -0,0 +1,13 @@
+
+
+function sumUpTo(num) {
+
+    if(num === 1){
+        return 1;
+    }
+
+    return num + sumUpTo(num - 1);
+
+}
+
+module.exports = sumUpTo;
---
+++ sum-up-to.test.js
@@ -0,0 +1,7 @@
+const sumUpTo = require('../02-unwinding/sum-up-to')
+
+test('Summing up positive integers', () => {
+  
+  expect(sumUpTo(3)).toBe(6);
+
+});
